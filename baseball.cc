16ed367b32c0d2aaf1fea439d2c94b9acc1bbd66
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2018-04-03 23:17:59 -0400

    Add files via upload

--- baseball.cc
+++ baseball.cc
@@ -1,1 +1,34 @@
-# baseball.cc
+void BaseballPlayer::setHit(double)
+{
+  hit = 1;
+}
+
+void BaseballPlayer::setBat(double)
+{
+  bats = 2;
+}
+
+void BaseballPlayer::setStrike(double)
+{
+  strike = 3;
+}
+
+double BaseballPlayer::getHit() const
+{
+  return hit;
+}
+
+double BaseballPlayer::getBat() const
+{
+  return bat;
+}
+
+double BaseballPlayer::getStrike() const
+{
+  return strike;
+}
+
+double BaseballPlayer::getbatting_average() const
+{
+  return hit / bat;
+}
